a5dd446376275f63303ae030fd732d98991a600b
QuentinBouvier <32564163+QuentinBouvier@users.noreply.github.com>
2019-01-15 21:01:19 +0100

    Changed transition between newgame and rules

--- src/components/Newgame.js
+++ src/components/Newgame.js
@@ -15,24 +15,24 @@ var Config = require("../app.conf.json");
 
 class Newgame extends Component {
     constructor(props) {
-            super(props);
+        super(props);
         this.state = {
-                    NewGame: false,
+            NewGame: false,
             Rules: false,
             GameId: 0
-            };
-            window.Echo = new Echo({
-                    broadcaster: 'socket.io',
+        };
+        window.Echo = new Echo({
+            broadcaster: 'socket.io',
 
             host: `${Config.API_URL}:${Config.SOCKETIO_PORT}`,
-                    client: Socketio,
-                    transports: ['websocket', 'polling', 'flashsocket'], //fix CORS issue
-                    auth: {
-                        headers: {
-                            Authorization: 'Bearer ' + localStorage.id_token,
-                        }
-                    }
-            });
+            client: Socketio,
+            transports: ['websocket', 'polling', 'flashsocket'], //fix CORS issue
+            auth: {
+                headers: {
+                    Authorization: 'Bearer ' + localStorage.id_token,
+                }
+            }
+        });
     }
 
     componentDidMount() {
@@ -41,41 +41,45 @@ class Newgame extends Component {
         const reqConfig = { headers: { "Accept": "application/json", "Authorization": 'Bearer ' + localStorage.id_token } };
 
         axios.post(`${API_URL}/api/game/start`, body, reqConfig)
-                    console.log("Error in API get call (Newgame)! ! ! ! !");
-                    console.log(error);
+            .then(this.initGame)
+            .catch((error) => {
+                console.log("Error in API get call (Newgame)! ! ! ! !");
+                console.log(error);
+            });
+    }
+
+    initGame = (res) => {
+        this.setGameId(res.data.channel_id); //have to store channel_id
+        console.log(localStorage);
+        console.log("Listening to channel : game." + localStorage.channel_id);
+        window.Echo.private('game.' + localStorage.game_id)
+            .listen('Website\\GameCreatedSuccess', (e) => {
+                // React Logic here
+                //Game's info (number of ruleset, and for each ruleset true or false)
+                console.log(e);
+                console.log(`Got confirmation for game`);
+                this.setState({
+                    Rules: true
+                })
             });
     }
 
     //Store channel_id
-    setChannel= channel_id => {
-            localStorage.setItem("channel_id", channel_id);
+    setGameId = id => {
+        localStorage.setItem("game_id", id);
     }
 
-    handleClick = (e) => {
-            console.log("You've cliked !!");
-            //connect to the private
-            window.Echo.private(localStorage.channel_id)
-            .listen('Website\\GameCreatedSuccess', (e) => {
-                    // React Logic here
-                    //Game's info (number of ruleset, and for each ruleset true or false)
-                    console.log(e);
-                    console.log("Channel works in the handleClick !! ");
-                    this.setState({
-                        Rules: true
-                    })
-            });
-    }
-
-    render(){
-        if(this.state.Rules === false) {
-            return(
-                    <div>
-                        <p> Choose wisely ...</p>
-                        <Button size="medium" variant="contained" className="User-button" color="primary" onClick={this.handleClick}> Ready now ? </Button>
-                    </div>
+    render() {
+        if (this.state.Rules === false) {
+            return (
+                <div>
+                    <svg className="spinner" width="65px" height="65px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
+                        <circle class="path" fill="none" strokeWidth="6" strokeLinecap="round" cx="33" cy="33" r="30"></circle>
+                    </svg>
+                </div>
             );
         } else {
-            return(
+            return (
                 <div>
                     <Rules />
                 </div>
