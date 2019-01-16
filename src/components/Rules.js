3af25b588ca6e6666f5f38a5fcb6d51f92dcfcb5
laetitiatran <33926760+laetitiatran@users.noreply.github.com>
2019-01-16 17:39:31 +0100

    list of button with round color

--- src/components/Rules.js
+++ src/components/Rules.js
@@ -5,10 +5,7 @@ import axios from 'axios';
 
 //import material UI
 import Button from '@material-ui/core/Button';
-
-//import component
-// import NewGame from './Newgame';
-// import User from './User';
+import Popover from '@material-ui/core/Popover';
 
 var Config = require("../app.conf.json");
 
@@ -20,7 +17,9 @@ class Rules extends Component {
             buttonPressed: 0,
             rulesets: '',
             gotdata: false,
-            open: false
+            open: false,
+            anchorEl: null,
+            soluce: [ ]
         };
         this.handleClick = this.handleClick.bind(this);
     }
@@ -42,38 +41,92 @@ class Rules extends Component {
         });
     }
 
-    handleOpen = (button) => {
+    handleOpen = (e) => {
         this.setState({
-            open: true
+            anchorEl: e.currentTarget,
+            open: true,
         });
+        console.log("test " + this)
+        
     };
 
     handleClose = (button) => {
         this.setState({
+            anchorEl: null,
             open: false
         });
     };
 
     render() {
+        const { anchorEl } = this.state;
+        const openApp = Boolean(anchorEl);
         let rulesets = []
         if (this.state.gotdata) {
+            console.log(this.state.rulesets)
             for (let rs of this.state.rulesets) {
-                rulesets.push(
-                    <li key={rs.id} className="solution-li">
-                        <Button
-                            value={rs.id}
-                            size="medium"
-                            variant="contained"
-                            className="User-button"
-                            color="primary"
-                            onClick={this.handleOpen}>
-                            {rs.combination}
-                        </Button>
-                    </li>
+                var colors = [
+                    rs.combination[0]? 'green': 'grey',
+                    rs.combination[1]? 'yellow': 'grey',
+                    rs.combination[2]? 'red': 'grey',
+                    rs.combination[3]? 'blue': 'grey'            
+                ]
+                var leds= []
 
+                for (let [index, color] of colors.entries()){
+                    leds.push(
+                        <span key={index} style={{color: color}}>&#9899;</span>
+                    )
+                }
+                var soluce = [
+                    rs.modules[0].solution == 1 ? 'true': 'false',
+                    rs.modules[1].solution == 1 ? 'true': 'false',
+                    rs.modules[2].solution == 1 ? 'true': 'false'
+                ]
+                
+                rulesets.push(
+                <li key={rs.id} className="solution-li">
+                <Button
+                    value={rs.id}
+                    size="medium"
+                    variant="contained"
+                    className="User-button"
+                    color="primary"
+                    onClick={this.handleOpen}>
+                {leds}
+                </Button>
+                <Popover
+                    open={openApp}
+                    anchorEl={anchorEl}
+                    onClose={this.handleClose}
+                    anchorOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'center',
+                    }}
+                    transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'center',
+                    }}>
+                  
+                </Popover>
+                </li>
                 )
+
+                
+                // if (soluce !== undefined){
+                //  return(
+                //      <p> {soluce}</p>
+                //  )
+                // } else {
+                //     return(
+                //     <p> Don't Press</p>
+
+                //     )
+                // }
+                // console.log(soluce);     
             }
+            
         }
+
 
         return (
             <div>
